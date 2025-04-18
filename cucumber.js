4abb7963b1f60d9b0909907d30e0e35c6450246d
wblair8689 <billblair@gmail.com>
2025-04-18 15:40:09 -0400

    Complete rebase and fix BDD testing with Cucumber

--- cucumber.js
+++ cucumber.js
@@ -1,17 +1,12 @@
 module.exports = {
   default: {
-    requireModule: [
-      'ts-node/register'
-    ],
-    require: [
-      'features/step_definitions/**/*.ts' // Path to your step definitions
-    ],
-    paths: [
-      'features/**/*.feature' // Path to your feature files
-    ],
-    format: [
-      'progress-bar'
-    ],
+    formatOptions: {
+      snippetInterface: 'synchronous'
+    },
+    requireModule: ['ts-node/register'],
+    require: ['features/step_definitions/**/*.ts'],
+    paths: ['features/**/*.feature'],
+    format: ['progress-bar'],
     publishQuiet: true
   }
 }; 
